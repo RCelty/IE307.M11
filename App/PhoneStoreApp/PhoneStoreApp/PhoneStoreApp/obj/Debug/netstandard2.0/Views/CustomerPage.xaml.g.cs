4244ccec6b87136c8aa76438523bb00b1106cc16
HieuBm <bmhieus07@gmail.com>
2021-12-09 16:27:04 +0700

    add customer page

--- App/PhoneStoreApp/PhoneStoreApp/PhoneStoreApp/obj/Debug/netstandard2.0/Views/CustomerPage.xaml.g.cs
+++ App/PhoneStoreApp/PhoneStoreApp/PhoneStoreApp/obj/Debug/netstandard2.0/Views/CustomerPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace PhoneStoreApp.Views {
     public partial class CustomerPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label lbUserName;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(CustomerPage));
+            lbUserName = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "lbUserName");
         }
     }
 }
