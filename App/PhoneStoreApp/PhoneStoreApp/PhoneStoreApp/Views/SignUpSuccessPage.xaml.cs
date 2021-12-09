64cdac78fb3418f47b66c4d4f1ba933f6e589562
HieuBm <bmhieus07@gmail.com>
2021-12-09 15:40:06 +0700

    add SignUp Page

---
+++ App/PhoneStoreApp/PhoneStoreApp/PhoneStoreApp/Views/SignUpSuccessPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhoneStoreApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpSuccessPage : ContentPage
+    {
+        public SignUpSuccessPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignUpSuccess_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
