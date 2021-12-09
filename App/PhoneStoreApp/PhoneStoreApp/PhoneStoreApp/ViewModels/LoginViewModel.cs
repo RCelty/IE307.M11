74df8416166e0164bcf61663830db409a572fae8
HieuBm <bmhieus07@gmail.com>
2021-12-10 00:21:27 +0700

    add login, signup viewmodel

---
+++ App/PhoneStoreApp/PhoneStoreApp/PhoneStoreApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PhoneStoreApp.Models;
+using PhoneStoreApp.Services;
+using PhoneStoreApp.Views;
+using Xamarin.Forms;
+
+
+namespace PhoneStoreApp.ViewModels
+{
+    class LoginViewModel : BaseViewModel
+    {
+        private string username;
+        public string UserName
+        {
+            get => username;
+            set
+            {
+                username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string userpassword;
+        public string UserPassword
+        {
+            get => userpassword;
+            set
+            {
+                userpassword = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region Command
+        public Command LostPasswordCommand { get; set; }
+        public Command LoginCommand { get; set; }
+        public Command SignUpCommand { get; set; }
+        #endregion
+
+        public LoginViewModel()
+        {
+            //LoadData();
+            LoginCommand = new Command(LoginCommandExecute, () => true);
+            LostPasswordCommand = new Command(LostPasswordCommandExecute, () => true); ;
+            SignUpCommand = new Command(SignUpCommandExecute, () => true);
+        }
+
+        public async void LostPasswordCommandExecute()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new LostPasswordPage());
+        }
+
+        public async void LoginCommandExecute()
+        {
+            var customer = await LoginServices.Instance.LoginAsync(UserName, UserPassword);
+            if (customer != null)
+            {
+                await App.Current.MainPage.Navigation.PushAsync(new MainViewPage());
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("","Tên đang nhập hoặc mật khẩu không đúng","OK");
+            }
+        }
+        public async void SignUpCommandExecute()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new SignUpPage());
+        }
+
+    }
+}
---
+++ App/PhoneStoreApp/PhoneStoreApp/PhoneStoreApp/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using PhoneStoreApp.Models;
+using PhoneStoreApp.Services;
+using PhoneStoreApp.Views;
+using Xamarin.Forms;
+
+namespace PhoneStoreApp.ViewModels
+{
+    class SignUpViewModel : BaseViewModel
+    {
+        private string customeremail;
+        public string CustomerEmail
+        {
+            get => customeremail;
+            set
+            {
+                customeremail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string customerdisplayname;
+        public string CustomerDisplayName
+        {
+            get => customerdisplayname;
+            set
+            {
+                customerdisplayname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string customername;
+        public string CustomerName
+        {
+            get => customername;
+            set
+            {
+                customername = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string customerpassword;
+        public string CustomerPassword
+        {
+            get => customerpassword;
+            set
+            {
+                customerpassword = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string customerconfirmpassword;
+        public string CustomerConfirmPassword
+        {
+            get => customerconfirmpassword;
+            set
+            {
+                customerconfirmpassword = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region Command
+        public Command GoBackOnClick { get; set; }
+        public Command SendFormSignUpCommand { set; get; }
+        #endregion
+
+        public SignUpViewModel()
+        {
+            
+
+            GoBackOnClick = new Command(GoBackOnClickExecute, () => true);
+            SendFormSignUpCommand = new Command(SendFormSignUpCommandExecute, () =>  true);
+        }
+
+        private async void GoBackOnClickExecute()
+        {
+            App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private async void SendFormSignUpCommandExecute()
+        {
+            Customer customer = new Customer() { Email = CustomerEmail, DisplayName = CustomerDisplayName, UserName = CustomerName, PassWord = CustomerPassword };
+
+            if (customer != null)
+            {
+                var isRegisterAlbe = await Services.LoginServices.Instance.IsRegisterAlbe(customer);
+                if (isRegisterAlbe == 1)
+                {
+                    var RegisterID = (int)await Services.LoginServices.Instance.Register(customer);
+                    if (RegisterID != -1)
+                    {
+                        App.Current.MainPage.Navigation.PushAsync(new SignUpSuccessPage());
+                    }
+                    else
+                    {
+                        App.Current.MainPage.DisplayAlert("Thông báo", "Đăng ký thông thành công", "Ok");
+                    }
+                }
+                else
+                {
+                    
+                    
+                        App.Current.MainPage.DisplayAlert("Thông báo", "Thông tin nhập chưa đúng", "Ok");
+                    
+
+                }
+
+            }
+            
+        }
+
+        
+    }
+}
