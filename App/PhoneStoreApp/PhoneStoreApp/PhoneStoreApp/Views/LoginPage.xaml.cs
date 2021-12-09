abc5e8978bf9226e3c897a922b8374f85ffbd1e0
HieuBm <bmhieus07@gmail.com>
2021-12-10 01:35:56 +0700

    fix login

--- App/PhoneStoreApp/PhoneStoreApp/PhoneStoreApp/Views/LoginPage.xaml.cs
+++ App/PhoneStoreApp/PhoneStoreApp/PhoneStoreApp/Views/LoginPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using PhoneStoreApp.ViewModels;
 
 namespace PhoneStoreApp.Views
 {
@@ -16,54 +17,8 @@ namespace PhoneStoreApp.Views
         public LoginPage()
         {
             InitializeComponent();
+            BindingContext = new LoginViewModel();
         }
-
-        private void btnLostPassword_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new LostPasswordPage());
-        }
-
-        private async void btnLogin_Clicked(object sender, EventArgs e)
-        {
-            const string emailRegex = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
-            const string passwordRegex = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
-
-            string txtEmail = txtLoginUserEmail.Text;
-            string txtPassword = txtLoginUserPassword.Text;
-
-
-            if (txtEmail == null || txtPassword == null)
-            {
-                await DisplayAlert("", "Tài khoản hoặc mật khẩu không đúng", "Ok");
-
-            }
-            else
-            {
-                // case: input error
-                if (!Regex.IsMatch(txtEmail, emailRegex) || !Regex.IsMatch(txtPassword, passwordRegex))
-                {
-
-                    if (!Regex.IsMatch(txtEmail, emailRegex))
-                    {
-                        lbUserMailError.Text = "Nhập sai dịnh dạng mail";
-                    }
-
-                    if (!Regex.IsMatch(txtPassword, passwordRegex))
-                    {
-                        lbUserPasswordError.Text = "Mật khảu gồm 8 ký tự ít nhất một số, và một chữ";
-                    }
-
-                }
-                else // case: input success, check exist account
-                {
-                    Navigation.PushAsync(new HomePage());
-                }
-            }
-        }
-
-        private void btnSignUpNewAccount_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new SignUpPage());
-        }
+        
     }
 }
