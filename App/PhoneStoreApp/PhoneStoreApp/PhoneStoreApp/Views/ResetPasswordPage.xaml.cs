d01e97b2025ce376523c53f8865c73f8f86f0cd7
HieuBm <bmhieus07@gmail.com>
2021-12-09 15:32:39 +0700

    add ResetPasswordPage

---
+++ App/PhoneStoreApp/PhoneStoreApp/PhoneStoreApp/Views/ResetPasswordPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhoneStoreApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResetPasswordPage : ContentPage
+    {
+        public ResetPasswordPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaveNewPassword_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
