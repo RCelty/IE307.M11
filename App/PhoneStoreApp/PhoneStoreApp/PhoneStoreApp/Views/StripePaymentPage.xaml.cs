7cac892cd0559288a096ea789140ecc197124eb0
arthur0409 <59772090+arthur0409@users.noreply.github.com>
2021-12-15 21:46:25 +0700

    Ngoc_comment_payment

---
+++ App/PhoneStoreApp/PhoneStoreApp/PhoneStoreApp/Views/StripePaymentPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using PhoneStoreApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhoneStoreApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StripePaymentPage : ContentPage
+    {
+        public StripePaymentPage(int toTalPrice)
+        {
+            BindingContext = new StripePaymentViewModel(toTalPrice);
+            InitializeComponent();
+        }
+    }
+}
